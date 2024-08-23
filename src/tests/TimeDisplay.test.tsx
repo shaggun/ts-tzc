ea88d34ff6fcfed82315feab7dabecdf43bb0830
shaggun <shaggynetworks@gmail.com>
2024-08-23 10:58:25 -0600

    Fix test

--- src/tests/TimeDisplay.test.tsx
+++ src/tests/TimeDisplay.test.tsx
@@ -4,5 +4,5 @@ import { TimeZones } from '../types/timeZones';
 
 test('renders time and timezone correctly', () => {
   render(<TimeDisplay time="12:34" timezone={TimeZones.UTC_PLUS_0} />);
-  expect(screen.getByText('Time in UTC +0: 12:34')).toBeInTheDocument();
+  expect(screen.getByText('12:34 UTC+0')).toBeInTheDocument();
 });
